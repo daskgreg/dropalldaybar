6f5f85063c4dfb03786c750710d4360e25ea00c6
daskgreg <dask.gregory@gmail.com>
2023-04-07 01:16:32 +0300

    Menu Creation

---
+++ template/js/menu.js
@@ -0,0 +1,17 @@
+
+function selectLanguage(){
+    var eng = document.getElementById("englishLanguage");
+    var greek = document.getElementById("greekLanguage");
+
+    if()
+}
+
+function showGreekMenu(){
+    var x = document.getElementById("greekMenu");
+
+    if( x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
